3ad246856b1aeb0c28ef4372a1c2f9c4b7ecdabb
Paulo Luan <pauloluan.inova@gmail.com>
2015-07-31 16:05:34 -0300

    added tests to search an address using a string.

--- test/scraper.js
+++ test/scraper.js
@@ -28,3 +28,27 @@ describe('Address from Postcode', function() {
 
     });
 });
+
+
+describe('Search address from a string', function() {
+    it('should search an address using a string', function(done) {
+
+        CEP.scrape('avenida paulista 1111', function(res) {
+            should(res).be.ok;
+            should(res.data).be.an.Array();
+            should(res.data).not.be.empty();
+
+            var result = res.data[0];
+
+            should(result.state).be.a.String();
+            should.equal(result.state, "SP", "State not equal as expected");
+            should.equal(result.postcode, "01311-920", "Postcode not equal as expected");
+
+            should.equal(result.address.name, "Avenida Paulista", "State not equal as expected");
+            should.equal(result.address.number, 1111, "Number is not equal as expected");
+
+            done();
+        });
+
+    });
+});
