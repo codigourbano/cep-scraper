fab5063316a9501cc40eaf9e6e9ed8e922d7dd09
Paulo Luan <pauloluan.inova@gmail.com>
2015-07-31 16:03:38 -0300

    added tests to get an address using a postcode

--- test/scraper.js
+++ test/scraper.js
@@ -1,8 +1,30 @@
 var module = require('../index'),
-    should = require('should');
+    should = require('should'),
+    CEP = require('../index');
 
 describe('#init', function() {
     it('should execute the first test :)', function(done) {
         done();
+    });
+});
+
+describe('Address from Postcode', function() {
+    it('should get an address from a postcode', function(done) {
+
+        CEP.scrape('01310-000', function(res) {
+            should(res).be.ok;
+            should(res.data).be.an.Array();
+            should(res.data).not.be.empty();
+
+            var result = res.data[0];
+
+            should(result.state).be.a.String();
+            should.equal(result.state, "SP", "State not equal as expected");
+
+            should.equal(result.address.name, "Avenida Paulista", "State not equal as expected");
+
+            done();
+        });
+
     });
 });
